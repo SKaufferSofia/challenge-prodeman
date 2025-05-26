f5d5aae9406d3946277b4107409818fc38f687d0
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2025-05-26 12:48:13 -0300

    cambio de estilo

--- src/components/UI/Checkbox.tsx
+++ src/components/UI/Checkbox.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import clsx from "clsx";
 
 const Checkbox = ({
   checked,
@@ -14,18 +13,9 @@ const Checkbox = ({
         type="checkbox"
         checked={checked}
         onChange={onChange}
-        className={clsx(
-          "cursor-pointer peer appearance-none w-5 h-5 bg-background dark:bg-background-dark rounded transition-colors",
-          "checked:border-redPrimary checked:bg-redPrimary"
-        )}
+        className="cursor-pointer peer appearance-none w-5 h-5 bg-background dark:bg-background-dark rounded transition-colors checked:border-redPrimary checked:bg-redPrimary"
       />
-      <span
-        className={clsx(
-          " absolute w-2.5 h-1 border-b-2 border-l-2 border-white rotate-[-45deg] scale-0",
-          "perspective-origin-top-left transition-transform duration-200 ease-in-out",
-          "peer-checked:scale-100"
-        )}
-      />
+      <span className="absolute w-2.5 h-1 border-b-2 border-l-2 border-white rotate-[-45deg] scale-0 perspective-origin-top-left transition-transform duration-200 ease-in-out peer-checked:scale-100" />
     </label>
   );
 };
