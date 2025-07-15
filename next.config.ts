8cd9ba0f1620f5b9129c07d892091304c61e0e8f
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2025-07-15 10:10:20 -0300

    block console log

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   compiler: {
-    removeConsole: false,
+    removeConsole: true,
   },
   images: {
     domains: [
